499eda4da821ac603bdcbbf5a864f31a38ac33f3
Yash Chitneni <yash.chitneni@gmail.com>
2015-03-24 15:23:55 -0500

    Homepage
    Initial commit to homepage for our 438 project

---
+++ homepage_script.js
@@ -0,0 +1,12 @@
+function initialize() {
+    var mapCanvas = document.getElementById('map-canvas');
+    var mapOptions = {
+        center: new google.maps.LatLng(44.5403, -78.5463),
+        zoom: 3,
+        minZoom: 3,
+        maxZoom: 9,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+    var map = new google.maps.Map(mapCanvas, mapOptions);
+}
+google.maps.event.addDomListener(window, 'load', initialize);
